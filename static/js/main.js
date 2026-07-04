3877453b7c2bf4c5cb7e2fbcdaa693b3871e106b
RIYANREZA <lorditshis@gmail.com>
2026-07-04 21:43:41 +0530

    sprint 1: personalized dashboard with dynamic greeting and date

---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+const welcomeMessage = document.getElementById("welcomeMessage");
+const currentDate = document.getElementById("currentDate");
+const currentTime = document.getElementById("currentTime");
+
+function updateDashboard() {
+
+    const today = new Date();
+
+    const hour = today.getHours();
+
+    let greeting = "";
+
+    if (hour < 12) {
+        greeting = "☀️ Good Morning";
+    }
+    else if (hour < 18) {
+        greeting = "🌤 Good Afternoon";
+    }
+    else {
+        greeting = "🌙 Good Evening";
+    }
+
+    welcomeMessage.textContent = `${greeting}, Riyan 👋`;
+
+    currentDate.textContent = today.toDateString();
+
+    currentTime.textContent = today.toLocaleTimeString();
+
+}
+
+updateDashboard();
